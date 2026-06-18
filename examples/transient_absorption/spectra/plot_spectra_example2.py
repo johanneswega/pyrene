d539f35e6c6695fa575bcea156208eb2e79e9434
Johannes Wega <wega@CL-C6C4P9JTYY.local>
2026-06-18 16:38:58 +0200

    fixed some bugs

--- examples/transient_absorption/spectra/plot_spectra_example2.py
+++ examples/transient_absorption/spectra/plot_spectra_example2.py
@@ -1,9 +1,9 @@
 from pyrene.transient_absorption import Spectra
 
-s = Spectra(files=['dA1.npy', 'dA2.npy'], x_cuts=[(400, 750)], delay=[1, 1], wn=False,
-            scatter=[None, (510, 540)], ma=[True, True],
-            norm=[True, True], norm_at=[417, 417], min_norm=[True, True], 
-            steady_state=[['abs.txt', (360, 700), -5.0, 'b', 'Abs.']])
+s = Spectra(files=['data/dA1.npy', 'data/dA2.npy'], x_cuts=[(400, 750)], delay=[1, 1], wn=False,
+            scatter=[None, (510, 540)], 
+            norm=[True], norm_at=[417, 417], 
+            steady_state=[['data/abs.txt', (360, 700), -5.0, 'b', 'Abs.']])
 
 # steady state spectra (optional) provided as lists [filename, cuts, scale factor, label]
 # absorption/emission spectra can be exported from respectrive classes with export method
